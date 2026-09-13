badf3eb65545231f853d30215026f2c32967d1cb
sbaron81 <sidneibaron@gmail.com>
2026-09-13 17:26:22 -0300

    Remove unnecessary test line from app.py

--- src/analytics-service/app.py
+++ src/analytics-service/app.py
@@ -10,7 +10,6 @@ from botocore.exceptions import NoCredentialsError, ClientError
 from flask import Flask, jsonify
 from dotenv import load_dotenv
 
-teste
 
 # Configura o logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
