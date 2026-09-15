cb75aa9f7da7dacd85293ba662010d262d790f2d
sbaron81 <sidneibaron@gmail.com>
2026-09-14 21:20:49 -0300

    refactor: remove unnecessary blank lines in app.py

--- src/analytics-service/app.py
+++ src/analytics-service/app.py
@@ -10,8 +10,6 @@ from botocore.exceptions import NoCredentialsError, ClientError
 from flask import Flask, jsonify
 from dotenv import load_dotenv
 
-
-
 # Configura o logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 log = logging.getLogger(__name__)
